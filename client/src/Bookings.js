f145443119b494af1480168022db5c590fbee77f
shriekster <shrieksterr@gmail.com>
2022-01-17 05:16:33 +0200

    work, work

--- client/src/Bookings.js
+++ client/src/Bookings.js
@@ -1,10 +1,51 @@
 import { useState, useEffect, useRef } from 'react';
 
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import MobileDatePicker from '@mui/lab/MobileDatePicker';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import Stack from '@mui/material/Stack';
+
 export default function Bookings() {
+  const [value, setValue] = useState(new Date());
 
   return (
     <div className='Bookings-root'>
-
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Stack spacing={3}>
+            <MobileDatePicker
+            label="For mobile"
+            value={value}
+            onChange={(newValue) => {
+                setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+            />
+            <DesktopDatePicker
+            label="For desktop"
+            value={value}
+            minDate={new Date('2017-01-01')}
+            onChange={(newValue) => {
+                setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+            />
+            <DatePicker
+            disableFuture
+            label="Responsive"
+            openTo="year"
+            views={['year', 'month', 'day']}
+            value={value}
+            onChange={(newValue) => {
+                setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+            />
+        </Stack>
+        </LocalizationProvider>
     </div>
   );
 }
+
